3df06a05307682f613ce9abb3062b8036f9abbfd
anil <anrai@deerwalk.com>
2016-06-17 16:00:56 +0545

    Added setSettingsValue() and small changes

--- src/tests/BaseTest.groovy
+++ src/tests/BaseTest.groovy
@@ -2,19 +2,62 @@ import geb.junit4.GebReportingTest
 import org.openqa.selenium.Dimension
 
 class BaseTest extends GebReportingTest {
-    static int numberOfTrials = 5
-    static int waitTime = 10
+    int numberOfTrials = 5
+    int waitTime = 10
     int count
 
-    static final boolean DEFAULT_DEBUG_PRINT = false
-    static final int DEFAULT_WINDOW_WIDTH = 1500
-    static final int DEFAULT_WINDOW_HEIGHT = 1000
+    boolean DEFAULT_DEBUG_PRINT = false
+    int DEFAULT_WINDOW_WIDTH = 1500
+    int DEFAULT_WINDOW_HEIGHT = 1000
 
     def setupSpec() {
-
+        setSettingsValue()
         def winSize = driver.manage().window().size
         driver.manage().window().setSize(new Dimension(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT))
+    }
 
+    def void setSettingsValue() {
+        String csvFile = "src/resources/testSettings.csv"
+        BufferedReader br = null
+        String line = ""
+        String cvsSplitBy = ","
+
+        try {
+            br = new BufferedReader(new FileReader(csvFile))
+            while ((line = br.readLine()) != null) {
+                // use comma as separator
+                String[] settings = line.split(cvsSplitBy)
+                println("Country [code= " + settings[0] + " , name=" + settings[1] + "]")
+
+                if(settings[0] == "numberOfTrials") {
+                    numberOfTrials = settings[1]
+                }
+                else if(settings[0] == "count") {
+                    count = settings[1]
+                }
+                else if(settings[0] == "WINDOW_WIDTH") {
+                    WINDOW_WIDTH = settings[1]
+                }
+                else if(settings[0] == "WINDOW_HEIGHT") {
+                    WINDOW_HEIGHT = settings[1]
+                }
+                else {
+                    break
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace()
+        } catch (IOException e) {
+            e.printStackTrace()
+        } finally {
+            if (br != null) {
+                try {
+                    br.close()
+                } catch (IOException e) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
 
--- src/tests/GebishOrgTest.groovy
+++ src/tests/GebishOrgTest.groovy
@@ -24,3 +24,8 @@ class GebishOrgTest extends BaseTest {
     }
 
 }
+
+
+
+
+
